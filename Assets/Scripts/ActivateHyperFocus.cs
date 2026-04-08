04567b1c80e3857ba043d8b09cf88644baa5467d
SoffeSof <Sofie.d.andersen@hotmail.dk>
2026-04-08 10:38:31 +0200

    ActivateHyperFocus

---
+++ Assets/Scripts/ActivateHyperFocus.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class ActivateHyperFocus : MonoBehaviour
+{
+    public float delay = 3f;     // Time before changing tag
+    public string targetTag = "TargetObject";     // Tag to assign
+
+    private void Start()
+    {
+        StartCoroutine(SetTagAfterDelay());
+    }
+
+    IEnumerator SetTagAfterDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        gameObject.tag = targetTag;
+    }
+}
